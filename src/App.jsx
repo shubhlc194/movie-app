25ccb94b8e9c76521529736e41ddf3aa1d17a0b5
shubhlc194 <shubhamkc194@gmail.com>
2025-11-20 14:02:26 +0530

    appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx'
 import MovieCard from './components/MovieCard.jsx'
 import { useDebounce } from 'react-use'
-import { getTrendingMovies, updateSearchCount } from './appwrite.js'
-
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
